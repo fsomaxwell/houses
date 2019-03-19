6d9fde3607ee4ee66d2e4b971fe334e991776875
fsomaxwell <fsomaxwell@mail.ru>
2019-03-19 21:41:02 +1000

    Добавлены папки

---
+++ detail/ul_fastovskaya_29/Лицензия на право управления домами/index.php
@@ -0,0 +1,4 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Лицензия на право управления домами в Приморском крае ");
+?><a href="/version2/site/houses/detail/IMG_0005.pdf" >Лицензия на осуществление предпринимательской деятельности по управлению многоквартирными домами</a><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ info/reports/month_reports/2019/index.php
@@ -0,0 +1,98 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Ежемесячные отчеты");
+?><a href="/version2/site/houses/detail/А. бульвар 18.xls" >Амурский бульвар, 18</a> 
+<div><a href="/version2/site/houses/detail/Александровская, 51.xlsx" >Александровский, 51</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Большая 5.xls" >Большая, 5</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Быстринская 191.xls" >Быстринская 19/1</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Gosp1" >Госпитальный, 1</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Даниловская, 16 .xlsx" >Даниловского, 16</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Даниловского 18 Г.xlsx" >Дангиловского 18Г </a></div>
+ 
+<div><a href="/version2/site/houses/detail/Evro4.xlsx" >Европейский, 4</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Запарина 87.xls" >Запарина, 87</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Знаменщикова 10.xlsx" >Знаменщикова, 10</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Истомина 60 .xls" >Истомина, 60</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Истомина, 62.xls" >Истомина, 62</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Карла Маркса 145.xls" >Карла Маркса, 145</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Карла Маркса 147.xls" >Карла Маркса 147</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Ким Ю Чена 63.xlsx" >Ким Ю Чена, 63</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Клубная 6А .xlsx" >Клубная 6А </a></div>
+ 
+<div><a href="/version2/site/houses/detail/Лазо 11.xlsx" >Лазо, 11</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Локомотивная, 6.xlsx" >Локомотивная, 6</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Льва Толстого 15.xls" >Льва Толстого, 15 </a></div>
+ 
+<div><a href="/version2/site/houses/detail/Марсовая 3.xlsx" >Марсовая, 3</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Молодежная 4 .xlsx" >Молодежная, 4</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Молодежная, 6.xlsx" >Молодежная, 6</a></div>
+ 
+<div><a href="/version2/site/houses/detail/МШ 9.xls" >Матвеевское шоссе, 9</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Осенняя 27.xls" >Осенняя, 27</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Пилотов 12.xlsx" >Пилотов, 12</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Пирогова 8Б.xlsx" >Пирогова, 8Б</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Постышева, 2.xlsx" >Постышева, 2</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Санитарная 7.xlsx" >Санитарная, 7</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Санитарная 16 .xls" >Санитарная, 16</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Санитарная 19 .xlsx" >Санитарная 19</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Sar, 2.xls" >Саратовский, 2</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Семашко, 20.xls" >Семашко, 20</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Серышева, 35.xlsx" >Серышева, 35</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Str5.xls" >Стрелочный проезд 5</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Стрельникова, 10 .xls" >Стрельникова, 10</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Stroit, 20.xls" >Строителей, 20</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Уборевича, 70.xlsx" >Уборевича, 70</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Хабаровская, 27А.xlsx" >Хабаровская, 27А</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Чехова, 6.xlsx" >Чехова, 6</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Шеронова 67.xlsx" >Шеронова, 67</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Школьная 4.xlsx" >Школьная, 4</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Sholom41.xls" >Шолом Алейхема 41А</a></div>
+ 
+<div><a href="/version2/site/houses/detail/Ясная 2А.xls" >Ясная 2А</a></div>
+ 
+<div><a href="/version2/site/houses/detail/KM1442A.xls" >Карла Маркса 144 2А</a></div>
+ 
+<div><a href="/version2/site/houses/detail/KM1442D(3).xls" >Карла Маркса 144 2Д</a></div>
+
+<div><a href="/version2/site/houses/detail/KM1442G (8).xls" >Карла Маркса 144 2Г</a></div>
+
+<div><a href="/version2/site/houses/detail/KM1442V (6).xls" >Карла Маркса 144 2В</a></div>
+
+<div><a href="/version2/site/houses/detail/MШ27.xls" >Матвеевское шоссе, 27</a></div>
+ <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
