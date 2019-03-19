7de9f73fc6332ff1663346f5cabf2e78d7c4e5f8
fsomaxwell <fsomaxwell@mail.ru>
2019-03-19 21:34:21 +1000

    Добавлены лицензии

--- detail/ul_fastovskaya_29/.left.menu.php
+++ detail/ul_fastovskaya_29/.left.menu.php
@@ -2,28 +2,28 @@
 $aMenuLinks = Array(
 	Array(
 		"Договор управления многоквартирным домом", 
-		"/version2/site/houses/detail/ul_fastovskaya_29/Договор управления многоквартирным домом/", 
+		"Договор управления многоквартирным домом/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Сметы доходов и расходов", 
-		"/version2/site/houses/detail/ul_fastovskaya_29/Сметы доходов и расходов/", 
+		"Сметы доходов и расходов/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Материалы собрания", 
-		"/version2/site/houses/detail/ul_fastovskaya_29/Материалы собрания/", 
+		"Материалы собрания/", 
 		Array(), 
 		Array(), 
 		"" 
 	),
 	Array(
 		"Лицензия на право управления домами", 
-		"/version2/site/houses/detail/ul_fastovskaya_29/Лицензия на право управления домами/", 
+		"Лицензия на право управления домами/", 
 		Array(), 
 		Array(), 
 		"" 
